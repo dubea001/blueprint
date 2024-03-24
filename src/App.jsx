ea6b1a1cd848df95bc6556a18c6ed49b7d9fa8c1
Dubea <dubea001@gmail.com>
2024-03-24 10:36:06 +0100

    testing hash routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+
 import React from 'react';
 import MainLayout from '../src/Layouts/MainLayout';
 import Homepage from '../src/Pages/Homepage';
@@ -9,7 +10,7 @@ import Reviewspage from '../src/Pages/Reviewspage';
 
 const App = () => {
   return (
-    <BrowserRouter basename='/blueprint'>
+    <Router>
       <Routes>
         <Route path='/' element={<MainLayout />}>
           <Route path='/' element={<Homepage />} />
@@ -19,8 +20,40 @@ const App = () => {
           <Route path='/reviews' element={<Reviewspage />} />
         </Route>
       </Routes>
-    </BrowserRouter>
+    </Router>
   );
 };
 
 export default App;
+
+
+
+
+
+
+// import { BrowserRouter, Routes, Route } from 'react-router-dom';
+// import React from 'react';
+// import MainLayout from '../src/Layouts/MainLayout';
+// import Homepage from '../src/Pages/Homepage';
+// import Getstartedpage from '../src/Pages/Getstartedpage';
+// import Aboutpage from '../src/Pages/Aboutpage';
+// import Services from '../src/Pages/Services';
+// import Reviewspage from '../src/Pages/Reviewspage';
+
+// const App = () => {
+//   return (
+//     <BrowserRouter basename='/blueprint'>
+//       <Routes>
+//         <Route path='/' element={<MainLayout />}>
+//           <Route path='/' element={<Homepage />} />
+//           <Route path='/getstarted' element={<Getstartedpage />} />
+//           <Route path='/about' element={<Aboutpage />} />
+//           <Route path='/services' element={<Services />} />
+//           <Route path='/reviews' element={<Reviewspage />} />
+//         </Route>
+//       </Routes>
+//     </BrowserRouter>
+//   );
+// };
+
+// export default App;
